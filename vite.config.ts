827c37b4f04e604e9631fce43f52784e318780a0
mailecfresh-max <mail.ecfresh@gmail.com>
2025-09-16 21:04:02 +0530

    Fix process is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  define: {
+    'process.env': {}
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
